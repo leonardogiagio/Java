823fc12fbee6546b8f56022fb58ffb603716d2b9
Leonardo <leonardogiagio@gmail.com>
2025-07-11 08:50:16 -0300

    funcoes matematicas

---
+++ src/Aintroducao/AEstruturaSequencial/Aula04FuncoesMatematicas.java
@@ -0,0 +1,12 @@
+package Aintroducao.AEstruturaSequencial;
+
+public class Aula04FuncoesMatematicas {
+    public static void main(String[] args) {
+        double A = 25.0;
+        double B = 3.0;
+
+        System.out.println("Raíz quadrada de " + A + " = " + Math.sqrt(A));
+        System.out.println("Potenciação: " + Math.pow(B, 3.0));
+        System.out.println("Valor absoluto: " + Math.abs(A/5.0));
+    }
+}
