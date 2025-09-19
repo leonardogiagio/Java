921bb85188f4b790238222646991e5f7d8e3ca99
Leonardo <leonardogiagio@gmail.com>
2025-09-19 00:41:14 -0300

    construtor / this / sobrecarga

---
+++ src/BorientacaoObjetos/Aintroducao/application/ProgramProductConstructor.java
@@ -0,0 +1,43 @@
+package BorientacaoObjetos.Aintroducao.application;
+
+import BorientacaoObjetos.Aintroducao.entities.ProductConstructor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramProductConstructor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        ProductConstructor product = new ProductConstructor(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println(product);
+
+        sc.close();
+    }
+}
